8ff5104e482c7eb8dfc298b79ef967e85ac37696
epedeene <127273844+epedeene@users.noreply.github.com>
2023-03-08 10:17:22 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '15976ce74f1353627b54382f0930bf7f',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/15976ce74f1353627b54382f0930bf7f/invoke.js"></scr' + 'ipt>');
